278565e1494f4b6b2e8aa62621981e12fff11d1a
Matheus Guimarães <mgguima95@gmail.com>
2022-09-20 14:39:53 -0300

    Using DTO to Data Transfer

--- src/main/java/com/alura/forum/controller/Topicos.java
+++ src/main/java/com/alura/forum/controller/Topicos.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alura.forum.controller.dto.TopicoDTO;
 import com.alura.forum.modelo.Curso;
 import com.alura.forum.modelo.Topico;
 
@@ -13,9 +14,9 @@ import com.alura.forum.modelo.Topico;
 public class Topicos {
 
 	@GetMapping("/topicos")
-	public List<Topico> listaTopicos() {
+	public List<TopicoDTO> listaTopicos() {
 		Topico topico = new Topico("Não consigo rodar o projeto", "Dá erro 404 not found", new Curso("Springboot para leigos", "Programação"));
 		
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
 	}
 }
--- src/main/java/com/alura/forum/modelo/Topico.java
+++ src/main/java/com/alura/forum/modelo/Topico.java
@@ -16,6 +16,7 @@ public class Topico {
 	private List<Resposta> respostas = new ArrayList<>();
 
 	public Topico(String titulo, String mensagem, Curso curso) {
+		this.id = 0l;
 		this.titulo = titulo;
 		this.mensagem = mensagem;
 		this.curso = curso;
