8929b9567480c667b5fa131dadb5fa13da7cd74c
Matheus Guimarães <mgguima95@gmail.com>
2022-09-26 16:11:12 -0300

    Using BeanValidations to validate fields

--- src/main/java/com/alura/forum/controller/Topicos.java
+++ src/main/java/com/alura/forum/controller/Topicos.java
@@ -2,13 +2,16 @@ package com.alura.forum.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.alura.forum.controller.dto.TopicoDTO;
 import com.alura.forum.form.TopicoForm;
@@ -39,8 +42,10 @@ public class Topicos {
 	}
 	
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm form) {
+	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
+		
+		return ResponseEntity.created(uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri()).body(new TopicoDTO(topico));
 	}
 }
--- src/main/java/com/alura/forum/form/TopicoForm.java
+++ src/main/java/com/alura/forum/form/TopicoForm.java
@@ -1,12 +1,20 @@
 package com.alura.forum.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.alura.forum.modelo.Topico;
 import com.alura.forum.repository.CursoRepository;
 
 public class TopicoForm {
 	
+	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
+	@NotNull @NotEmpty @Length(min = 10)
 	private String mensagem;
+	@NotNull @NotEmpty
 	private String nomeCurso;
 	
 	public String getTitulo() {
